1d4b16658c6ca77da5eaf5a81dbfb7e40d79bdd6
matthewhorn95 <121875846+matthewhorn95@users.noreply.github.com>
2023-01-15 21:19:11 +0200

    anecdotes step 2
    finished step 2, added style to html anecdote, fixed random index generator to not get same index as current

--- part-1/anecdotes/src/App.js
+++ part-1/anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css';
 
 const App = () => {
   const anecdotes = [
@@ -10,14 +11,38 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+
+  const countInit = new Array(anecdotes.length).fill(0)
+
+  const [countArr, setCount] = useState(countInit)
    
   const [selected, setSelected] = useState(0)
 
+  const updateCount = () => {
+    const copy = [...countArr]
+    copy[selected] += 1
+    setCount(copy)
+  }
+
+  const getRand = () => {
+    const rand = Math.floor(Math.random() * anecdotes.length)
+    if (rand === selected) {
+      return((rand+1)%anecdotes.length)
+    }
+    return (rand)
+  }
+
   return (
-    <div>
-      <p>{anecdotes[selected]}</p>
-      <Button onClick = {() => anecdotes[setSelected(Math.floor(Math.random() * anecdotes.length))]} text = "Generate Random Anecdote" />
-    </div>
+    <>
+      <div>
+        <p class="text">{anecdotes[selected]}</p>
+        <p>has {countArr[selected]} votes</p>
+      </div>
+      <div>
+        <Button onClick = {() => updateCount()} text = "Vote" />
+        <Button onClick = {() => setSelected(getRand())} text = "Generate random anecdote" />
+      </div>
+    </>
   )
 }
 
