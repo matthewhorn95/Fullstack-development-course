66520d7f1d7bb48cdd647c0f67f1c3750b5d4d0a
matthewhorn95 <121875846+matthewhorn95@users.noreply.github.com>
2023-01-15 16:41:48 +0200

    unicafe steps 1.7-1.8

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -6,9 +6,39 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = (val) => {
+    setGood(val)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick = {() => setGood(good + 1)} text = "good" />
+      <Button onClick = {() => setNeutral(neutral + 1)} text = "neutral" />
+      <Button onClick = {() => setBad(bad + 1)} text = "bad" />
+      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {good + bad + neutral}/>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad , all}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad)/all}</p>
+      <p>positive {good/all}</p>
     </div>
   )
 }
