cc7aacb79fc1bf3bd158b11b13660745797f67a8
matthewhorn95 <121875846+matthewhorn95@users.noreply.github.com>
2023-02-19 14:52:51 +0200

    phonebook backend step 2 revision
    changed address from ./api/info to ./info

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -32,7 +32,7 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
-app.get('/api/info', (request, response) => {
+app.get('/info', (request, response) => {
     currDate = new Date()
     console.log(currDate)
     response.send(`<p>Phonebook has info for ${persons.length} people </p> 
