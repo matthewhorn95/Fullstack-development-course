42719b29c919ab6bd4ba656ac06294e3aa40773c
matthewhorn95 <121875846+matthewhorn95@users.noreply.github.com>
2023-05-21 08:30:43 +0300

    test that button toggles blog visibility

--- part-5/bloglist-frontend/src/components/Blog.test.js
+++ part-5/bloglist-frontend/src/components/Blog.test.js
@@ -1,27 +1,52 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen, within } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog.js'
 
-test('default blog', () => {
-    const blog = {
-        title: 'Art of the Ballad',
-        author: 'Chet Baker',
-        url: 'fakeurl.org',
-        likes: 38,
-        user: {
-            name: 'Cheese Rat'
+
+describe('Blog tests', () => {
+
+    let container
+
+    beforeEach(() => {
+
+        const blog = {
+            title: 'Art of the Ballad',
+            author: 'Chet Baker',
+            url: 'fakeurl.org',
+            likes: 38,
+            user: {
+                name: 'Cheese Rat'
+            }
         }
-    }
 
-    const { container } = render(<Blog blog={blog} />)
-    const div = container.querySelector('.blog')
+        container = render(< Blog blog={blog} />).container
+    })
 
-    const toggleable = container.querySelector('.toggleable')
+    test('default blog', () => {
 
-    expect(div).toHaveTextContent('Art of the Ballad')
-    expect(div).toHaveTextContent('Chet Baker')
+        const div = container.querySelector('.blog')
 
-    expect(toggleable).toHaveStyle('display: none')
+        const toggleable = container.querySelector('.toggleable')
 
+        expect(div).toHaveTextContent('Art of the Ballad')
+        expect(div).toHaveTextContent('Chet Baker')
+
+        expect(toggleable).toHaveStyle('display: none')
+
+    })
+
+    test('toggle activates visibility', async () => {
+
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const toggleable = container.querySelector('.toggleable')
+
+        expect(toggleable).toHaveStyle('display: block')
+
+    })
 })
+
